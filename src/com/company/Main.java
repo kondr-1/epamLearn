c6449abf8b5750942a2aa7bd8f23de540f13d36b
Anton Kondralev <motovolg-1@yandex.ru>
2020-06-03 00:41:03 +0400

    variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,16 @@ public class Main {
 
     public static void main(String[] args)
     {
-    System.out.println("Введите число");
-        Scanner scanner = new Scanner(System.in); //читает из консоли введеное число
-        int a = Scanner.nextInt(); // переменная (контейнер хранение данных) пишем туда значение полченные из сканера
-        System.out.println("a = " +a); //выводим значение переменной на экрон
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
+
+//    System.out.println("Введите число");
+//        Scanner scanner = new Scanner(System.in); //читает из консоли введеное число
+//        int a = Scanner.nextInt(); // переменная (контейнер хранение данных) пишем туда значение полченные из сканера
+//        System.out.println("a = " +a); //выводим значение переменной на экрон
     }
 }
