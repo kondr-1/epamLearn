c6b89b14e15772e5e428caf294d6bea97fb787a9
Anton Kondralev <motovolg-1@yandex.ru>
2020-06-03 00:47:59 +0400

    variables
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,20 @@ public class Main {
 
     public static void main(String[] args)
     {
+
     System.out.println("Введите операцию");
 System.out.println("1. Сложение");
         System.out.println("2. Вычитание");
         System.out.println("3. Умножение");
         System.out.println("4. Деление");
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        System.out.println("Введите первое число");
+        int a = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int b = scanner.nextInt();
 
-
-
+//    System.out.println("Введите число");
 //        Scanner scanner = new Scanner(System.in); //читает из консоли введеное число
 //        int a = Scanner.nextInt(); // переменная (контейнер хранение данных) пишем туда значение полченные из сканера
 //        System.out.println("a = " +a); //выводим значение переменной на экрон
