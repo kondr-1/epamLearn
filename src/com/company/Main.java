c12cda0e2e909b8b67a725c0ec92f13353c78c92
Anton Kondralev <motovolg-1@yandex.ru>
2020-06-02 23:02:29 +0400

    merge conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
     System.out.println("Введите число");
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("a = "+a);
     }
 }
