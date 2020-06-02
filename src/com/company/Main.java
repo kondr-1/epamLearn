3fb05685791667819a91e0ad407372195285b107
Anton Kondralev <motovolg-1@yandex.ru>
2020-06-02 22:57:03 +0400

    the first code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
     System.out.println("Введите число");
-	// write your code here
+        Scanner scanner = new Scanner(System.in); //читает из консоли введеное число
+        int a = Scanner.nextInt(); // переменная (контейнер хранение данных) пишем туда значение полченные из сканера
+        System.out.println("a = " +a); //выводим значение переменной на экрон
     }
 }
